5be69b34a046b9bca4bf63036d9bd56a0d0b6b95
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2019-11-27 14:14:43 +0800

    02-functional

--- src/main/java/io/humourmind/flightschedule/FlightScheduleApplication.java
+++ src/main/java/io/humourmind/flightschedule/FlightScheduleApplication.java
@@ -2,12 +2,13 @@ package io.humourmind.flightschedule;
 
 import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -22,30 +23,22 @@ public class FlightScheduleApplication {
 		SpringApplication.run(FlightScheduleApplication.class, args);
 	}
 
-	private final Source source;
-
 	private String[] flights = { "SQ02", "SQ502", "SQ405", "GA03", "GA501" };
 	private String[] destinations = { "SIN", "CGK", "BXW", "BDO", "SFO", "PCB" };
 	private String[] bayIds = { "B01", "B22", "B40", "B32", "B20" };
 	private long[] delay = { 0, 5, 10, 20, 30 };
 
-	FlightScheduleApplication(Source source) {
-		this.source = source;
-	}
-
 	@Scheduled(fixedDelay = 1000)
-	public void generateSchedule() {
+	@Bean
+	public Supplier<FlightSchedule> generateSchedule() {
 		LocalDateTime now = LocalDateTime.now();
-		Random rand = new Random();
-		this.source.output()
-				.send(MessageBuilder.withPayload(
-						FlightSchedule.builder().flightNo(flights[rand.nextInt(5)])
-								.destination(destinations[rand.nextInt(6)])
-								.sta(now.plusMinutes(delay[rand.nextInt(5)]))
-								.ata(now.plusMinutes(delay[rand.nextInt(5)]))
-								.scheduledBayId(bayIds[rand.nextInt(5)])
-								.actualBayId(bayIds[rand.nextInt(5)]).build())
-						.build());
+		final Random rand = new Random();
+		return () -> FlightSchedule.builder().flightNo(flights[rand.nextInt(5)])
+				.destination(destinations[rand.nextInt(6)])
+				.sta(now.plusMinutes(delay[rand.nextInt(5)]))
+				.ata(now.plusMinutes(delay[rand.nextInt(5)]))
+				.scheduledBayId(bayIds[rand.nextInt(5)])
+				.actualBayId(bayIds[rand.nextInt(5)]).build();
 	}
 
 }
